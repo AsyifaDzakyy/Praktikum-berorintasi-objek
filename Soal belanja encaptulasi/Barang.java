2d7ee569fd3b9ad364aea13de7b006a4a4504c54
AsyifaDzakyy <161543937+AsyifaDzakyy@users.noreply.github.com>
2024-03-18 14:38:16 +0700

    Add files via upload

---
+++ Soal belanja encaptulasi/Barang.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Barang {
+
+    private String namaBarang;
+
+    private int hargaBarang;
+
+    private int jumlahBarang;
+
+    public Barang(String namaBarang, int hargaBarang, int jumlahBarang) {
+        this.namaBarang = namaBarang;
+        this.hargaBarang = hargaBarang;
+        this.jumlahBarang = jumlahBarang;
+    }
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+}
